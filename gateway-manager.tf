efbf9ce18d724a80e42abe169d26210aba653700
liyangau <li.yang@konghq.com>
2024-04-19 14:24:17 +1000

    add a test cp

--- gateway-manager.tf
+++ gateway-manager.tf
@@ -10,6 +10,18 @@ resource "konnect_gateway_control_plane" "tf-cp" {
   }
 }
 
+resource "konnect_gateway_control_plane" "tf-test-cp" {
+  name         = "tf-test-cp"
+  description  = "This is the first control plane created by terraform"
+  cluster_type = "CLUSTER_TYPE_CONTROL_PLANE"
+  auth_type    = "pki_client_certs"
+
+  labels = {
+    env         = "test"
+    provisioner = "terraform"
+  }
+}
+
 resource "konnect_gateway_control_plane" "tf-k8s-cp" {
   name         = "tf-k8s-cp"
   description  = "This is the K8S control plane created by terraform"
