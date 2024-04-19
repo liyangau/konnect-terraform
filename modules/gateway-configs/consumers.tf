3f64499862c9dd573d2f7863e991d32496f32880
liyangau <li.yang@konghq.com>
2024-04-19 23:58:12 +1000

    change folder structure

--- modules/gateway-configs/consumers.tf
+++ modules/gateway-configs/consumers.tf
@@ -1,6 +1,5 @@
 resource "konnect_gateway_consumer" "alice" {
   username         = "alice"
-  custom_id        = "alice"
   control_plane_id = var.control_plane_tf_cp.id
 }
 
@@ -18,6 +17,25 @@ resource "konnect_gateway_key_auth" "alice_keyauth" {
   control_plane_id = var.control_plane_tf_cp.id
 }
 
+resource "konnect_gateway_consumer" "alex" {
+  username         = "alex"
+  control_plane_id = var.control_plane_tf_cp.id
+}
+
+resource "konnect_gateway_basic_auth" "alex_basicauth" {
+  username = var.alex_credentials.username
+  password = var.alex_credentials.password
+
+  consumer_id      = konnect_gateway_consumer.alex.id
+  control_plane_id = var.control_plane_tf_cp.id
+}
+
+resource "konnect_gateway_key_auth" "alex_keyauth" {
+  key              = var.alex_credentials.api_key
+  consumer_id      = konnect_gateway_consumer.alex.id
+  control_plane_id = var.control_plane_tf_cp.id
+}
+
 variable "alice_credentials" {
   type = object({
     api_key  = string
@@ -27,8 +45,24 @@ variable "alice_credentials" {
 
   default = {
     api_key  = "alice"
-    username = "alice-test"
-    password = "demo"
+    username = "alice"
+    password = "alice-pw"
+  }
+
+  sensitive = true
+}
+
+variable "alex_credentials" {
+  type = object({
+    api_key  = string
+    username = string
+    password = string
+  })
+
+  default = {
+    api_key  = "alex"
+    username = "alex"
+    password = "alex-pw"
   }
 
   sensitive = true
