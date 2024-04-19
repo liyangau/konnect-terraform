c8fa9b2c02631a38bc55d32d858f65f97c694647
Li Yang <71299093+liyangau@users.noreply.github.com>
2024-04-19 13:19:30 +1000

    Update mesh-manager.tf

--- mesh-manager.tf
+++ mesh-manager.tf
@@ -1,6 +1,6 @@
 # # Create a new Control Plane
 resource "konnect_mesh_control_plane" "tf-global-cp" {
-  count       = 0
+  count       = var.exclude_mesh_control_plane ? 0 : 1
   name        = "tf-global-cp"
   description = "This is the mesh control plane created by terraform"
 
