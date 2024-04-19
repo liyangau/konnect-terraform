fe703737228f5d4dc949d48ad186bd6eb11e1740
liyangau <li.yang@konghq.com>
2024-04-19 16:50:59 +1000

    update alice

--- consumers.tf
+++ consumers.tf
@@ -11,3 +11,9 @@ resource "konnect_gateway_basic_auth" "alice_basicauth" {
   consumer_id      = konnect_gateway_consumer.alice.id
   control_plane_id = konnect_gateway_control_plane.tf-cp.id
 }
+
+resource "konnect_gateway_key_auth" "alice_keyauth" {
+  key = "alice"
+  consumer_id      = konnect_gateway_consumer.alice.id
+  control_plane_id = konnect_gateway_control_plane.tf-cp.id
+}
