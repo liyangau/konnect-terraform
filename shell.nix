7f78d8d8046e097870b392cbdd35120c26bb8009
liyangau <li.yang@konghq.com>
2024-04-22 21:11:49 +1000

    update mesh variable

--- shell.nix
+++ shell.nix
@@ -8,6 +8,6 @@ pkgs.mkShell {
   packages = with pkgs; [ terraform ];
   shellHook = ''
     export TF_VAR_cp_region=au
-    export TF_VAR_exclude_mesh_control_plane=true
+    export TF_VAR_exclude_mesh_control_plane=false
   '';
 }
