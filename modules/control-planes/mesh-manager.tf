b9b1deba6cc86f3a36e956e5ba746c959a22ec2e
liyangau <li.yang@konghq.com>
2024-04-22 21:28:57 +1000

    create mesh global cp

--- modules/control-planes/mesh-manager.tf
+++ modules/control-planes/mesh-manager.tf
@@ -1,10 +1,8 @@
 resource "konnect_mesh_control_plane" "tf_global_cp" {
-  count       = var.exclude_mesh_control_plane ? 0 : 1
   name        = "tf-global-cp"
   description = "This is the mesh control plane created by terraform"
 
   labels = {
-    env         = "test"
     provisioner = "terraform"
   }
 }
--- shell.nix
+++ shell.nix
@@ -8,6 +8,5 @@ pkgs.mkShell {
   packages = with pkgs; [ terraform ];
   shellHook = ''
     export TF_VAR_cp_region=au
-    export TF_VAR_exclude_mesh_control_plane=false
   '';
 }
