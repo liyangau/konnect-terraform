f94f59fb146988f66ab25e6f9ad531e4db645313
liyangau <li.yang@konghq.com>
2024-07-26 18:40:41 +1000

    remove mesh manager outside gateway control planes

---
+++ mesh-manager.tf
@@ -0,0 +1,8 @@
+# resource "konnect_mesh_control_plane" "tf_global_cp" {
+#   name        = "tf-global-cp"
+#   description = "This is the mesh control plane created by terraform"
+
+#   labels = {
+#     provisioner = "terraform"
+#   }
+# }
--- modules/control-planes/mesh-manager.tf
+++
@@ -1,8 +0,0 @@
-resource "konnect_mesh_control_plane" "tf_global_cp" {
-  name        = "tf-global-cp"
-  description = "This is the mesh control plane created by terraform"
-
-  labels = {
-    provisioner = "terraform"
-  }
-}
